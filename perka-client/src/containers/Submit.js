1f5d24380e0f2bd1ec23e7e3fc73f90ebb39a9fa
Daniel Park <dpeace926@gmail.com>
2017-09-26 23:52:38 -0400

    import react-file-base64

--- perka-client/src/containers/Submit.js
+++ perka-client/src/containers/Submit.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+// import FileBase64 from './react-file-base64.js';
 
 class Submit extends Component {
   constructor () {
@@ -13,8 +14,36 @@ class Submit extends Component {
       projects: [],
       source: '',
       resume: '',
+      // files: [],
     }
   }
+
+  // getFiles(files){
+  //   this.setState({
+  //     files: files
+  //   })
+  // }
+
+  // convertToBase64 = () => {
+  //   //Read File
+  //   var selectedFile = document.getElementById("inputFile").files;
+  //   //Check File is not Empty
+  //   if (selectedFile.length > 0) {
+  //       // Select the very first file from list
+  //       var fileToLoad = selectedFile[0];
+  //       // FileReader function for read the file.
+  //       var fileReader = new FileReader();
+  //       var base64;
+  //       // Onload of file read the file content
+  //       fileReader.onload = function(fileLoadedEvent) {
+  //           base64 = fileLoadedEvent.target.result;
+  //           // Print data in console
+  //           console.log(base64);
+  //       };
+  //       // Convert data to base64
+  //       fileReader.readAsDataURL(fileToLoad);
+  //   }
+  // }
 
   handleInput = (event) => {
     let name = event.target.name
@@ -23,8 +52,10 @@ class Submit extends Component {
     })
   }
 
-  handleSubmit = () => {
-    fetch('http://localhost:3000/api/v1/perka', {
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch('https://api.perka.com/1/communication/job/apply', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -38,16 +69,11 @@ class Submit extends Component {
         explanation: this.state.explanation,
         projects: this.state.projects.split(", "),
         source: this.state.source,
-        resume: this.state.resume,
+        resume: this.state.files,
       })
     })
     .then( res => res.json() )
-    .then( data => this.setState({
-      weatherData: this.state.weatherData.concat([data]),
-      addCity: !this.state.addCity,
-      input: '',
-      error: '',
-    }))
+    .catch( (error) => console.log(error.response))
   }
 
   render = () => {
@@ -60,14 +86,34 @@ class Submit extends Component {
         </div>
       )
     })
-    console.log(Object.keys(this.state))
+    // console.log(Object.keys(this.state))
 
     return (
       <div>
         <form onSubmit={this.handleSubmit} id="perka-form">
           {inputs}
+          {/* <p className="text-center"> *) Try To Upload Some Image~</p>
+          <FileBase64
+            multiple={ true }
+            onDone={ this.getFiles.bind(this) } /> */}
           <input type="submit" value="SUBMIT"></input>
         </form>
+
+        {/* <div className="text-center">
+          { this.state.files.map((file,i) => {
+            return <img key={i} src={file.base64} />
+          }) }
+          <img src="" />
+        </div>
+
+        { this.state.files.length != 0 ?
+          <div>
+            <h3 className="text-center mt-25">Callback Object</h3>
+            <div className="pre-container">
+              <pre>{ JSON.stringify(this.state.files, null, 2) }</pre>
+            </div>
+          </div>
+        : null } */}
       </div>
     )
   }
